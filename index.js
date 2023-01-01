5dccdcbe945dc2f763a7f2bd10f25c23421f9546
maurimtz <102897627+maurimtz@users.noreply.github.com>
2023-01-01 04:10:41 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+import Swal from 'sweetalert2'
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function () {
+    Swal.fire({
+        title: 'Sabia que ibas a decir que si :3',
+        imageUrl: 'cora.png'
+    });
+});
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('mouseover', function () {
+    const randomX = parseInt(Math.random()*100);
+    const randomY = parseInt(Math.random()*100);
+    noBtn.style.setProperty('top',randomY+'%');
+    noBtn.style.setProperty('left',randomX+'%');
+    noBtn.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
+})
